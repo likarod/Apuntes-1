7dde4939ef08c1e35d362dd9f0ec1b1dee53c25c
Likaro-nav <63605973+Likaro-nav@users.noreply.github.com>
2022-05-24 17:30:13 +0200

    Apuntes 4

---
+++ _pruebaLanding/JS/index.js
@@ -0,0 +1,22 @@
+// var numero1 = parseInt(prompt("Solicitar el primer número"));
+// var comandos = prompt("Introduce la operación a realizar. Suma, resta, multiplicación, división."); 
+//     console.log(comandos)
+// var numero2 = parseInt(prompt("Solicitar el segundo número"));
+
+
+// switch(comandos){
+//     case "+": 
+//         alert(numero1+numero2)
+//     break;
+//     case "-":
+//         alert(numero1-numero2)
+//     break;
+//     case "*":
+//         alert(numero1*numero2)
+//     break;
+//     case "/":
+//         alert(numero1/numero2)
+//     break;
+//     default:
+//         alert("No se reconoce la operación.");
+// }
